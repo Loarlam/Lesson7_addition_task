df4d7ebd9b6fbd42298bcd516fa05cf56d41fd76
Loarlam <messiah02@mail.ru>
2020-02-22 13:09:49 +0300

    Добавил структуру с пользовательским конструктором, полями и методом на вывод полей. В классе Program обратился к структуре, передав данные конструктору по умолчанию, и вывел информацию о ноутбуке на экран.

--- Addition_task/Program.cs
+++ Addition_task/Program.cs
@@ -7,11 +7,34 @@ using System;
 
 namespace Addition_task
 {
+    struct Notebook
+    {
+        string _model, _manufacturer;
+        double _price;
+
+        public Notebook(string model, string manufacturer, double price)
+        {
+            _model = model;
+            _manufacturer = manufacturer;
+            _price = price;
+        }
+
+        public void DisplaysTheContentsOfTheFiledsOnTheScreen()
+        {
+            Console.WriteLine("Информация о ноутбуке:");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"модель {_model}\nпроизводитель {_manufacturer}\nцена {_price}");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Notebook notebook = new Notebook("Aspire v3-771g", "Acer", 24500.50);
+            notebook.DisplaysTheContentsOfTheFiledsOnTheScreen();
+
+            Console.ReadKey();
         }
     }
 }
